4cfd03e22f109b13156c500fa830890fdc75880c
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-03-18 13:03:07 +0300

    18.03

--- it-incubator-todolist-ts-01/src/app.tsx
+++
@@ -1,7 +0,0 @@
-// нет
-<h3 className={"buttonTitle"}>
-<EditableSpan callBack={(title) => editTitleTodolistHendler(props.todolistId, title)}
-              title={props.title}/>
-<button onClick={onClickHandlerForDeletTodo}>x</button>
-
-</h3>
